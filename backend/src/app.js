55d732d61e739e8c67927c63f232175e2b50853a
Pwnkdm <pawanrajkadam@gmail.com>
2024-08-31 12:00:35 -0600

    cors error fixed

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,8 +5,8 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    // origin: process.env.CORS_ORIGIN,
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
+    // origin: 'http://localhost:3000',
     credentials: true,
 }));
 app.use(express.json({limit:"16kb"}));
