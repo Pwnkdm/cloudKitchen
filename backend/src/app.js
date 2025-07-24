789498a66d7f1b9a04565250dc61fbe61e2588d8
pwnkdm <pawanrajkadam@gmail.com>
2025-07-24 10:50:59 +0530

    changes made

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
-    // origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN,
     // origin: 'http://localhost:3000',
     credentials: true,
   })
